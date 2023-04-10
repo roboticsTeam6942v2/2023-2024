774ad6785a6531cd7ffab5436b87b504c64e5975
EricLottman <rick821044@gmail.com>
2023-04-10 02:48:47 -0400

    fixed function calls of drive()

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/subsystemTestAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/subsystemTestAuto.java
@@ -7,23 +7,36 @@ import org.firstinspires.ftc.teamcode.system.testy;
 @Autonomous(name="subsystem autonomous",group="tests")
 
 public class subsystemTestAuto extends LinearOpMode {
+    String stage = "declaring";
     @Override
     public void runOpMode() {
         // initializes and hardware-maps robot
         testy robot = new testy(hardwareMap);
+        stage = "initializing";
 
         waitForStart();
 
         while (opModeIsActive()) {
+            stage = "running";
+            // test drive()
+            robot.drivetrain.drive("f", 10, .5);
+            robot.drivetrain.drive("b", 10, .5);
+            robot.drivetrain.drive("l", 10, .5);
+            robot.drivetrain.drive("r", 10, .5);
+            robot.drivetrain.drive("fr", 10, .5);
+            robot.drivetrain.drive("bl", 10, .5);
+            robot.drivetrain.drive("fl", 10, .5);
+            robot.drivetrain.drive("br", 10, .5);
 
-            robot.drivetrain.drive("foward", 10, .5);
-            robot.drivetrain.drive("backward", 10, .5);
-            robot.drivetrain.drive("left", 10, .5);
-            robot.drivetrain.drive("right", 10, .5);
-            robot.drivetrain.drive("diagonal fr", 10, .5);
-            robot.drivetrain.drive("diagonal bl", 10, .5);
-            robot.drivetrain.drive("diagonal fl", 10, .5);
-            robot.drivetrain.drive("diagonal br", 10, .5);
+            stage = "rotating";
+            // test possible rotation code
+            robot.drivetrain.rotate(-90, .5);
+
+
+            stage = "end of autonomous";
+            while (stage.equals("end of autonomous")) {
+                sleep(1);
+            }
         }
     }
 }
